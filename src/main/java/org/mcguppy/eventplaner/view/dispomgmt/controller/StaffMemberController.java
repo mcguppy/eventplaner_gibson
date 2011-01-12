584813d1f8e9340ebe4d1d66bfd6ee7c0e1296b7
unknown <TGDMEST5@.corproot.net>
2011-01-12 08:14:46 +0100

    20110112_0814

--- src/main/java/org/mcguppy/eventplaner/view/dispomgmt/controller/StaffMemberController.java
+++ src/main/java/org/mcguppy/eventplaner/view/dispomgmt/controller/StaffMemberController.java
@@ -13,82 +13,80 @@ import org.mcguppy.eventplaner.business.dispomgmt.domain.StaffMember;
 @Named
 public class StaffMemberController implements Serializable {
 
-        @EJB
-        StaffMemberService service;
+    @EJB
+    StaffMemberService service;
+    private Long id;
+    private StaffMember instance;
+    @Inject
+    private Conversation conversation;
 
-	private Long id;
-	private StaffMember instance;
-
-	@Inject
-	private Conversation conversation;
-
-	public StaffMember getInstance() {
-		if (instance == null) {
-			if (id != null) {
-				instance = loadInstance();
-			} else {
-				instance = createInstance();
-			}
-		}
-		return instance;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public StaffMember loadInstance() {
-		return service.find(getId());
-	}
-
-	public StaffMember createInstance() {
-		try {
-			return StaffMember.class.newInstance();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public boolean isManaged() {
-		return getInstance().getId() != null;
-	} 
-
-	public String save() {
-		if (isManaged()) {
-			service.edit(instance);
-		} else {
-			service.create(instance);
-		}
-		conversation.end();
-		return "saved";
-	}
-
-	public String cancel() {
-		conversation.end();
-		return "cancelled";
-	}
-
-	public void initConversation() {
-		if (conversation.isTransient()) {
-			conversation.begin();
-		}
-	}
-
-	public String delete() {		
-		System.out.println("Deleting "+getInstance());
-		service.remove(instance);
-		conversation.end();
-		return "deleted";		
-	}
-
-        @Produces
-        @Named
-        public List<StaffMember> getStaffMembers() {
-            return service.findAll();
+    public StaffMember getInstance() {
+        if (instance == null) {
+            if (id != null) {
+                instance = loadInstance();
+            } else {
+                instance = createInstance();
+            }
         }
+        return instance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public StaffMember loadInstance() {
+        return service.find(getId());
+    }
+
+    public StaffMember createInstance() {
+        try {
+            return StaffMember.class.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean isManaged() {
+        return getInstance().getId() != null;
+    }
+
+    public String save() {
+        if (isManaged()) {
+            service.edit(instance);
+        } else {
+            service.create(instance);
+        }
+        conversation.end();
+        return "saved";
+    }
+
+    public String cancel() {
+        conversation.end();
+        return "cancelled";
+    }
+
+    public void initConversation() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+        }
+    }
+
+    public String delete() {
+        System.out.println("Deleting " + getInstance());
+        service.remove(instance);
+        conversation.end();
+        return "deleted";
+    }
+
+    @Produces
+    @Named
+    public List<StaffMember> getStaffMembers() {
+        return service.findAll();
+    }
 }
